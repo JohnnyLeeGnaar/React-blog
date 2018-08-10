dbf964180735e6351bcda452f8789491b55b5808
bigbear <mrvuckov@gmail.com>
2018-08-10 22:18:30 +0200

    Pulling data from jsonplaceholder API. Currently available only in console

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+   super(props);
+   this.state = {
+     data: null,
+   };
+   }
+  
+   componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/todos/')
+  .then(response => response.json())
+  .then(data => this.setState({ data }))
+
+}
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        {console.table(this.state.data)}  
       </div>
     );
   }
+  
 }
-
 export default App;
