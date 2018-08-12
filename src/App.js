88f233a6771d0341b7ac2a967ef14db48c106dc2
bigbear <mrvuckov@gmail.com>
2018-08-12 19:02:01 +0200

    Undefined

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class App extends Component {
   constructor(props){
    super(props);
    this.state = {
-     data: null,
+     posts: [],
    };
    }
-  
+   
    componentDidMount(){
   fetch('https://jsonplaceholder.typicode.com/todos/')
   .then(response => response.json())
-  .then(data => this.setState({ data }))
+  .then(data => { let posts = data.response.map((post) => {return(
+    <div key={post.id}> </div>
 
+  )
+  })
+  this.setState({posts:posts});
+  console.log("state", this.state.posts);
+  })
 }
+
+  
   render() {
     return (
       <div>
